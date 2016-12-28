9f2801f69da1b290e0c15ead75bec725e1029e47
rodnyjoseph <rjoseph3@ggc.edu>
2016-12-28 04:18:34 +0000

    Laravel server side validation and javascript client side validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', '| create new post')
 
+@section('stylesheet')
+
+    {!! Html::style('css/parsley.css') !!}
+
+@endsection
+
 @section('content')
 
     <div class="row">
@@ -9,13 +15,13 @@
             <h1>Create New Post</h1>
             <hr>
             
-            {!! Form::open(array('route' => 'posts.store')) !!}
+            {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
                 
                 {{ Form::label('title', 'Title:') }}
-                {{ Form::text('title', null, array('class' => 'form-control')) }}
+                {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
                 
                 {{ Form::label('body', "Post Body:") }}
-                {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+                {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
                 
                 {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px')) }}
                 
@@ -24,4 +30,8 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    {!! Html::script('js/parsley.min.js')  !!}
 @endsection
